d9f8027b2cc8be137923f850ad147b72f9f44124
kuttel <kuttel@192.168.0.75>
2022-10-02 10:11:49 -0300

    merged

--- src/main/java/com/example/demo/Demo3Application.java
+++ src/main/java/com/example/demo/Demo3Application.java
@@ -12,3 +12,17 @@ public class Demo3Application {
 	}
 
 }
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo3Application.class, args);
+		//this is a test
+	}
+
+}
