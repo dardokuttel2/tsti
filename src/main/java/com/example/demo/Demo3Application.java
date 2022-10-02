6ab8b66cc806c5cc55cb72b4864dfa887b4f3cf9
kuttel <kuttel@192.168.0.75>
2022-10-02 10:22:14 -0300

    ok, se resuelve el conflicto

--- src/main/java/com/example/demo/Demo3Application.java
+++ src/main/java/com/example/demo/Demo3Application.java
@@ -8,20 +8,6 @@ public class Demo3Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Demo3Application.class, args);
-		//this is another test
-	}
-
-}
-package com.example.demo;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class Demo3Application {
-
-	public static void main(String[] args) {
-		SpringApplication.run(Demo3Application.class, args);
 		//this is a test
 	}
 
